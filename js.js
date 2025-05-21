127c5db2e1139d6a78dab3b52efa2a7e3f574d9d
Eloise-W <2007569@apps.nsd.org>
2025-05-21 16:37:20 +0000

    The next edit with colored buttons

--- js.js
+++ js.js
@@ -3,7 +3,17 @@ var LIST = [
     "Banana Sorbet ",
     "Sandwitch"
 ];
-for(let i = 0; i<3; i++){//can use let or var
-    alert("Frozen burritoes "+LIST[i]);
 
+var BTN = document.getElementById("mybutton");
+var theAction = function(event){
+    alert("Hello! You clicked the button");
+    console.log(event);
+}
+for(let i = 0; i<3; i++){//can use let or var
+    var newBTN = BTN.cloneNode(true);
+    newBTN.innerHTML = LIST[i];
+    document.body.append(newBTN);
+    alert("Check me out "+ LIST[i]);
+    newBTN.addEventListener("click", theAction);
+    
 }
